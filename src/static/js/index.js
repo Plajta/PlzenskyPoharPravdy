b023ec7afbd543d0090964deb9b4d6bb9b7a64d1
salivo <andrijsalivon@gmail.com>
2024-01-19 17:06:14 +0100

    multifile

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,9 +1,18 @@
+
+function Menu() {
+    return (<div className="map_cont">Menu</div>)
+}
 function App() {
     return(
-    <>
-    <h1>hello world</h1>
-    </>);
+        <div>  
+            <Menu />
+            <MapShow />
+            <h1>hello</h1>
+        </div>
+    );
 }
+
+
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
---
+++ src/static/js/map.js
@@ -0,0 +1,8 @@
+
+function MapShow(){
+    return (
+        <div className="page">
+            <h1>Hello!</h1>
+        </div>
+        );
+}
