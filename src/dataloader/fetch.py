c8263ec9a6c71ea93b33354ea71a08f2a0140b3e
HelloWorld7894 <danpojhan@protonmail.com>
2024-01-19 18:07:26 +0100

    for some reason the fetching is so fuckin slow

---
+++ src/dataloader/fetch.py
@@ -0,0 +1,15 @@
+from urllib.request import urlretrieve
+import json
+import os
+
+#get abs path
+fetch_config_path = os.getcwd()[:os.getcwd().index("PlzenskyPoharPravdy") + len("PlzenskyPoharPravdy")] + "/data/other_data/fetch_config.json"
+img_data_path = os.getcwd()[:os.getcwd().index("PlzenskyPoharPravdy") + len("PlzenskyPoharPravdy")] + "/data/img_data/"
+
+fetch_config = open(fetch_config_path)
+fetch_config_data = json.load(fetch_config)
+
+#read image data
+for obj in fetch_config_data["img_data"]:
+   print(obj)
+   urlretrieve(obj["link"], os.path.join(img_data_path, obj["dir"] + obj["name"]))
