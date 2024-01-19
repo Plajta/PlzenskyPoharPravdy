d3d94ee5a4ed418d21de28be241ea0873da1552f
salivo <andrijsalivon@gmail.com>
2024-01-19 16:30:46 +0100

    webrace

---
+++ src/static/js/index.js
@@ -0,0 +1,10 @@
+function App() {
+    return(
+    <>
+    <h1>hello world</h1>
+    </>);
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
