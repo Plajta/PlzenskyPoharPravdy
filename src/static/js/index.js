50e18b45ff36a95f246e6c0e664a3b8dafd8618b
Sles Usagi <usagisles@mail.com>
2024-01-19 21:36:55 +0100

    back

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -5,15 +5,15 @@ function Menu() {
 
         
         <div className="typetextdiv">
-            <i class="fa-solid fa-magnifying-glass search"></i>
+            <i className="fa-solid fa-magnifying-glass search"></i>
             <input type="text" className="typetext" id="mesto" name="mesto" placeholder="Search"/>
             </div>
         
         <input type="text" className="typetext" id="dataset" name="dataset"/>
 
         <div>
-            <button onclick="sendMessage(nukede 40 50)">nukede</button>
-            <button onclick="sendMessage(generate data)">generate</button>
+            <button >nukede</button>
+            <button >generate</button>
         </div>
     </div>
     )
