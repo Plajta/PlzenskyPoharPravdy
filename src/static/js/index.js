bd54c38a2d86b287e81f386cb5bd2b25807d3ad9
salivo <andrijsalivon@gmail.com>
2024-01-20 01:50:13 +0100

    fully worked map

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -4,6 +4,50 @@ function nukedeHandler(){
     socket.emit('generate', marker.getLatLng());
 }
 
+const nukemapChange = (setValue) => {
+    setValue(true)
+    console.log(bomb_longitude)
+    console.log(bomb_latitude)
+
+    map.remove()
+    map = L.map('map').setView({lng: bomb_longitude, lat: bomb_latitude}, 16);
+
+    L.tileLayer(`https://api.mapy.cz/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${API_KEY}`, {
+        minZoom: 0,
+        maxZoom: 19,
+        attribution: '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+    }).addTo(map);
+
+
+
+    // add_circle([latitude, longitude])
+
+    marker_bomb = L.marker([bomb_latitude, bomb_longitude], { draggable: true }).addTo(map);
+    marker_bomb.bindPopup("<b>Vaši bomba</b>").openPopup()
+    marker_bomb.on('dragend', function (event) {
+        var marker = event.target;
+        bomb_latitude = marker_bomb.getLatLng().lat;
+        bomb_longitude = marker_bomb.getLatLng().lng;
+        marker.bindPopup("<b>Vaši bomba</b>").openPopup()
+    });
+}
+
+const factsmapChange = (setValue) => {
+    setValue(false)
+
+    console.log(longitude)
+    console.log(latitude)
+    map.remove()
+    map = L.map('map').setView({lng: longitude, lat: latitude}, 16);
+    L.tileLayer(`https://api.mapy.cz/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${API_KEY}`, {
+        minZoom: 0,
+        maxZoom: 19,
+        attribution: '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+    }).addTo(map);
+    marker_gps = L.marker([latitude, longitude], { draggable: false }).addTo(map);
+    marker_gps.bindPopup("<b>Vaši poloha</b>").openPopup()
+}
+
 
 function Menu() {
     const [value, setValue] = React.useState(false);
@@ -11,8 +55,8 @@ function Menu() {
         
     <div className="menu">
         <div className="menu_change_buttons">
-            <div className={!value && "menu_change_buttons menu_change_button_active"} onClick={() => {setValue(false)}}>Farts</div>
-            <div className={value && "menu_change_buttons menu_change_button_active"} onClick={() => {setValue(true)}}>Nukede</div>
+            <div className={!value && "menu_change_buttons menu_change_button_active"} onClick={() => factsmapChange(setValue)}>Farts</div>
+            <div className={value && "menu_change_buttons menu_change_button_active"} onClick={() => nukemapChange(setValue)}>Nukede</div>
         </div>
         {!value && (
             <>
--- src/static/js/map.js
+++ src/static/js/map.js
@@ -6,29 +6,7 @@ const options = {
     maximumAge: 0,
 };
 
-var longitude = 13.381112
-var latitude = 49.736953
 
-var map = undefined
-
-var userIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-    text: "vaše poloha"
-});
-
-var targetIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
 
 window.onload = () => {
 
@@ -58,26 +36,25 @@ window.onload = () => {
     that shows a clickable logo.
     See https://leafletjs.com/reference.html#control
     */
-    const LogoControl = L.Control.extend({
-    options: {
-        position: 'bottomleft',
-    },
+    // const LogoControl = L.Control.extend({
+    // options: {
+    //     position: 'bottomleft',
+    // },
 
-    onAdd: function (map) {
-        const container = L.DomUtil.create('div');
-        const link = L.DomUtil.create('a', '', container);
+    // onAdd: function (map) {
+    //     const container = L.DomUtil.create('div');
+    //     const link = L.DomUtil.create('a', '', container);
 
-        link.setAttribute('href', 'http://mapy.cz/');
-        link.setAttribute('target', '_blank');
-        link.innerHTML = '<img src="https://api.mapy.cz/img/api/logo.svg" />';
-        L.DomEvent.disableClickPropagation(link);
+    //     link.setAttribute('href', 'http://mapy.cz/');
+    //     link.setAttribute('target', '_blank');
+    //     link.innerHTML = '<img src="https://api.mapy.cz/img/api/logo.svg" />';
+    //     L.DomEvent.disableClickPropagation(link);
 
-        return container;
-    },
-    });
-
-    // finally we add our LogoControl to the map
-    new LogoControl().addTo(map);
+    //     return container;
+    // },
+    // });
+    // new LogoControl().addTo(map);
+    
 }
 
 
@@ -87,63 +64,31 @@ function watch(pos) {
     if (crd.longitude != longitude && crd.latitude != latitude){
         longitude = crd.longitude
         latitude = crd.latitude
-
         console.log(longitude)
         console.log(latitude)
+        if (!marker_bomb){
+            bomb_longitude = longitude
+            bomb_latitude = latitude
 
-        map.remove()
-        map = L.map('map').setView({lng: longitude, lat: latitude}, 16);
+            
 
-        L.tileLayer(`https://api.mapy.cz/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${API_KEY}`, {
-            minZoom: 0,
-            maxZoom: 19,
-            attribution: '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
-        }).addTo(map);
+            map.remove()
+            map = L.map('map').setView({lng: longitude, lat: latitude}, 16);
 
-        add_icon([latitude, longitude], userIcon, "vaše poloha")
+            L.tileLayer(`https://api.mapy.cz/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${API_KEY}`, {
+                minZoom: 0,
+                maxZoom: 19,
+                attribution: '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+            }).addTo(map);
 
-        //test
-        add_icon([latitude + 0.05, longitude + 0.05], targetIcon, "místo odpálení bomby")
-        add_polygon([
-            [latitude, longitude],
-            [latitude - 5, longitude - 5],
-            [latitude - 5, longitude + 5]
-        ], map)
 
-        add_circle([latitude, longitude])
-        marker = L.marker([latitude, longitude], { draggable: true }).addTo(map);
 
-        // Event listener to handle marker dragend event
-        marker.on('dragend', function (event) {
-            var marker = event.target;
-            var position = marker.getLatLng();
-            console.log('Marker dragged to:', position);
-        });
-        marker.bindPopup("<b>Tohle to je </b><br>místo odpálení bomby").openPopup()
+            // add_circle([latitude, longitude])
+
+            marker_gps = L.marker([latitude, longitude], { draggable: false }).addTo(map);
+            marker_gps.bindPopup("<b>Vaši poloha</b>").openPopup()
+        }
     }
 }
 
-function add_polygon(coords, map){
-    L.polygon(coords).addTo(map);
-}
-
-function add_circle(coords){
-    L.circle(coords, {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 3000
-    }).addTo(map);
-}
-
-function add_icon(coords, marker_color, text = ""){
-    L.marker(coords, {icon: marker_color})
-    .bindTooltip(text, 
-    {
-        permanent: true, 
-        direction: 'right'
-    }).
-    addTo(map)
-    .openPopup()
-}
 
