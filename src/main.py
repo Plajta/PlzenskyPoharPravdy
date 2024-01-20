d384222d0aac6a1e60519a6ea309c2f0d0ddf899
HelloWorld7894 <danpojhan@protonmail.com>
2024-01-20 02:23:51 +0100

    there are nukes on your way andri

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import json
+import os
+
+#variables
+nuke_config_path = os.getcwd()[:os.getcwd().index("PlzenskyPoharPravdy") + len("PlzenskyPoharPravdy")] + "/data/other_data/nuke_config.json"
+nukes = []
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    #loading nukes
+    nuke_config = open(nuke_config_path)
+    nuke_config_data = json.load(nuke_config)
+    for nuke in nuke_config_data["nukes"]:
+        nukes.append({
+            "name": nuke["name"],
+            "energy": str(nuke["energy"]) + "kt",
+            "value": nuke["name"].replace(" ", "")
+        })
+
+    print(nukes)
+    return render_template('index.html', nuke_data=nukes)
 
 @socketio.on('generate')
 def handle_message(data):
