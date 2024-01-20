ee4f3d849e0316fc0d97c36f858ada5eb2e7de28
HelloWorld7894 <danpojhan@protonmail.com>
2024-01-20 05:38:55 +0100

    cropping works

--- src/dataloader/img_loader.py
+++ src/dataloader/img_loader.py
@@ -71,23 +71,43 @@ resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 threshold_value = 150
 binary_image = np.where(resized < threshold_value, 0, 255).astype(np.uint8)
 
-coordinates = np.column_stack(np.where(binary_image == 0))
-leftmost = tuple(coordinates[0])[0]
-rightmost = tuple(coordinates[-1])[0]
-topmost = tuple(np.min(coordinates, axis=0))[1]
-botmost = tuple(np.max(coordinates, axis=0))[1]
+contours, _ = cv2.findContours(binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+max_contour = max(contours, key=cv2.contourArea)
+filtered_contours = [cnt for cnt in contours if cnt is not max_contour]
 
-cropped_img = img[leftmost:rightmost, topmost:botmost]
+contour_data = []
+for contour in filtered_contours:
+    extLeft = tuple(contour[contour[:, :, 0].argmin()][0])[0]
+    extRight = tuple(contour[contour[:, :, 0].argmax()][0])[0]
+    extTop = tuple(contour[contour[:, :, 1].argmin()][0])[1]
+    extBot = tuple(contour[contour[:, :, 1].argmax()][0])[1]
+    contour_data.append([extLeft, extRight, extTop, extBot])
+    
+np_cnt_data = np.array(contour_data)
+print(np_cnt_data)
+
+leftmost = round((np_cnt_data[:, 0].min() / resized.shape[0]) * img.shape[0])
+rightmost = round((np_cnt_data[:, 1].max() / resized.shape[0]) * img.shape[0])
+topmost = round((np_cnt_data[:, 2].min() / resized.shape[1]) * img.shape[1])
+botmost = round((np_cnt_data[:, 3].max() / resized.shape[1]) * img.shape[1])
+
+print(leftmost)
+print(rightmost)
+print(topmost)
+print(botmost)
+
+cropped_img = img[topmost:botmost, leftmost:rightmost]
 print(cropped_img.shape)
 
-#TODO: does not seem to work, showing too small pixels
-threshold_value = 150
-cropped_img = np.where(cropped_img < threshold_value, 0, 255).astype(np.uint8)
+#TODO: too big to threshold
+#threshold_value = 150
+#cropped_img = np.where(cropped_img < threshold_value, 0, 255).astype(np.uint8)
 
 print(cropped_img.dtype)
 print(img.dtype)
 
-imsave("test.tif", np.array(cropped_img).astype(np.uint8))
+imsave("test.tif", cropped_img)
+
 exit()
 
 cv2.imshow("test", cropped_img)
