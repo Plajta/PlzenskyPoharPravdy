18963ade0350e1d4816dacd6c1f36b9c1d254144
Vesek <venda.straka@gmail.com>
2024-01-23 19:40:24 +0100

    Add hash checking

--- src/dataloader/fetch.py
+++ src/dataloader/fetch.py
@@ -1,26 +1,43 @@
 from urllib.request import urlretrieve
 import json
-import os
+import os.path
 import hashlib
 
 #own libs
 import img_loader
 
 #get abs path
-fetch_config_path = os.getcwd()[:os.getcwd().index("PlzenskyPoharPravdy") + len("PlzenskyPoharPravdy")] + "/data/other_data/fetch_config.json"
-img_data_path = os.getcwd()[:os.getcwd().index("PlzenskyPoharPravdy") + len("PlzenskyPoharPravdy")] + "/data/img_data/"
+main_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+fetch_config_path = os.path.join(main_dir, "data/other_data/fetch_config.json")
+img_data_path = os.path.join(main_dir, "data/img_data/")
 
 with open(fetch_config_path) as fetch_config:
     data = json.load(fetch_config)
 keys = data.keys()
 
+errors = False
+
 for key in keys:
     for file in data[key]:
         path = f"data/{key}/{file['name']}"
+        desired_hash = file["sha1"]
+        print(f"{path}: {desired_hash}", end=" ", flush=True)
         if not os.path.isfile(path):
+            print(f"- downloading", end=" ", flush=True)
             urlretrieve(file["link"], path)
+        if desired_hash == hashlib.sha1(open(path,'rb').read()).hexdigest():
+            print("✓")
         else:
-            print(f"{path}: {hashlib.sha1(open(path,'rb').read()).hexdigest()}")
+            errors = True
+            print("✗ - REMOVING")
+            os.remove(path)
+            mod = path[:-4]+"_mod.tif"
+            if os.path.exists(mod):
+                os.remove(mod)
+            
 
 #modify images
-img_loader.normalize_and_modify()
+if errors:
+    print("\033[91mRUN AGAIN, CURRUPTED FILES FOUND!")
+else:
+    img_loader.normalize_and_modify()
